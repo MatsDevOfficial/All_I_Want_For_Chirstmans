8cfdbdc3c14f5495578ea3c5bd8317c0655d9e25
Movermans1 <619401@olz.nl>
2024-11-27 13:26:49 +0100

    Added saving a list

--- actions.js
+++ actions.js
@@ -8,6 +8,13 @@ document.getElementById('wishlist-form').addEventListener('submit', function(eve
         const listItem = document.createElement('li');
         listItem.textContent = itemText;
         wishlist.appendChild(listItem);
+        saveItem(itemText);
         itemInput.value = '';
     }
 });
+
+function saveItem(item) {
+    let items = JSON.parse(localStorage.getItem('wishlist')) || [];
+    items.push(item);
+    localStorage.setItem('wishlist', JSON.stringify(items));
+}
